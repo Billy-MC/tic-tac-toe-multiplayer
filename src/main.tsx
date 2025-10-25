ed1ebc4bab7936c915e9c240bfdddc9a7a1ef5da
Billy <billychuim@gmail.com>
2025-10-25 13:33:59 +1100

    feat: create the boundary for firebase

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from 'styled-components'
 import App from '@/App'
 import { theme } from '@/styles/theme'
 import { GlobalStyles } from '@/styles/GlobalStyles'
-import ErrorBoundary from './components/ErrorBoundary/index'
+import { ErrorBoundary } from './components/ErrorBoundary/index'
 const container = document.getElementById('root')
 if (!container) {
 	throw new Error('Root container "#root" not found')
