f613830b125677a572105d6e15f5b279329f27e4
Billy <billychuim@gmail.com>
2025-10-29 21:57:49 +1100

    docs: update comment

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,6 +1,7 @@
 import 'styled-components'
 import type { Theme } from '@/styles/theme'
 
+// Extend styled-components’ built-in DefaultTheme.
 declare module 'styled-components' {
 	export interface DefaultTheme extends Theme {}
 }
